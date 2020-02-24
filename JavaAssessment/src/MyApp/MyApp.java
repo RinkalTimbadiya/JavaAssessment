13dd949813535d52ef50d94122e2d02d0a5c92ce
admin <admin@LAPTOP-EUQ4NAFC>
2020-02-24 21:00:07 +0530

    Call the Method

--- JavaAssessment/src/MyApp/MyApp.java
+++ JavaAssessment/src/MyApp/MyApp.java
@@ -1,14 +1,14 @@
 package MyApp;
-
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class MyApp {
-
+	
 	Map<Integer,Integer> myApp;
     Map<Integer,List<Integer>> followers;
     Deque<Integer> PostList;
@@ -23,7 +23,6 @@ public class MyApp {
     public void postMessage(int userId, int tweetId) {
     	myApp.put(tweetId,userId);
     	PostList.addFirst(tweetId);
-      //  System.out.println(myApp);
     }
     
     /**Retrieve the 10 most recent post ids in the user's news feed. Each item in the news feed
@@ -56,7 +55,6 @@ public class MyApp {
             	List<Integer> ls = new ArrayList<>();
                 ls.add(followeeId);
                 followers.put(followerId,ls);
-                System.out.println(followers);
             }
             else{
                 followers.get(followerId).add(followeeId);
@@ -75,4 +73,90 @@ public class MyApp {
         }
         
         
+        public static void main(String args[]){  
+       	 MyApp obj = new MyApp();
+   		 for(int i=0;i<10;i++) {
+   			 int userId = 0;
+   			 int tweetId = 0;
+   			 int followerId = 0;
+   			 int followeeId = 0;
+       	    		 System.out.println(" Enter 1 to posts a new message");
+       	    		 System.out.println(" Enter 2 to news feed");
+       	    		 System.out.println(" Enter 3 to follows user");
+       	    		 System.out.println(" Enter 4 to unfollows user");
+       	    		 System.out.println("<-------Select Your Option-------->");
+       	    		 Scanner sc = new Scanner(System.in);
+       	    		 String  Option = sc.nextLine();
+       	    		 
+       	    		 if(Option.equals("1")) {
+       	    			 System.out.println("<------Post a new massage------>");
+       	    			 
+       	    			  
+       	    			   try {
+       	    				   System.out.println("Enter user ID :");
+       	    				   userId = sc.nextInt();
+       	    				   System.out.println("Enter Post Message Id :");
+       	    				   tweetId = sc.nextInt();
+       	    				} catch (Exception e) {
+       	    					// TODO: handle exception
+       	    					System.out.println(e);
+       	    				}
+       	    				
+       	    				obj.postMessage(userId, tweetId);
+       	    		 }
+       	    		 
+       	    		 else if(Option.equals("2")) {
+       	    			 System.out.println("<-------News Feed----->");
+       	    			 
+       	    			 System.out.println("Enter User ID : ");
+       	    			   try {
+       	    				   userId = sc.nextInt();
+       	    				} catch (Exception e) {
+       	    					// TODO: handle exception
+       	    					System.out.println(e);
+       	    				}
+       	    			 
+       	    			 List<Integer> param_2 = obj.getNewsFeed(userId);
+       	    			 System.out.println("Result : "+param_2);
+       	    			 
+       	    			 
+       	    		 }
+       	    		 
+       	    		 else if(Option.equals("3") || Option.equals("4")) {
+       	    			 
+   		    			 if(Option.equals("3")) {
+   		    				 System.out.println("<-------Follow user------> ");
+   		    			 }else if(Option.equals("4"))
+   		    			 {
+   		    				 System.out.println("<-------Unfollow user------>");
+   		    			 }
+   		    			 
+   	    			   try {
+   	    				   System.out.println("Follower User Id :");
+   	    				   followerId = sc.nextInt();
+   	    				   System.out.println("Followee User Id :");
+   	    				   followeeId = sc.nextInt();
+   	    				} catch (Exception e) {
+   	    					// TODO: handle exception
+   	    					System.out.println(e);
+   	    				}
+   		    				
+   	    				 if(Option.equals("3")) {
+   	    					 obj.follow(followerId, followeeId);
+   	    				 }else if(Option.equals("4"))
+   	    				 {
+   	    					 obj.unfollow(followerId, followeeId);
+   	    				 }
+   		    				  
+       	    		 }
+       	    		 else
+       	    		 {
+       	    			System.out.println("Select Right option"); 
+       	    		 }
+       	    		 i=1;
+       	    		 continue;
+   	
+   		 }
+       	}
+
 }
